7fa31ee096d650185315aba8897f26ba641f47ec
Moong <tmdals9154@gmail.com>
2024-08-11 17:39:15 +0900

    ✨ [Feat] react-daum-postcode API 연동 (주소 검색)

--- src/app/(user)/signup/address/page.tsx
+++ src/app/(user)/signup/address/page.tsx
@@ -1,65 +1,50 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Toaster } from '@/components/ui/toaster';
-import { toast } from '@/components/ui/use-toast';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { Label } from '@/components/ui/label';
 import Image from 'next/image';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { useState } from 'react';
+import { useDaumPostcodePopup } from 'react-daum-postcode';
 
-//& 수정 필요 (zod) : destructive 글로벌 css 넣어주면 됨
-const FormSchema = z.object({
-    name: z.string().min(2, {
-        message: '올바른 이메일 형식이 아닙니다.',
-    }),
-    email: z.string().min(2, {
-        message: '비밀번호 제대로 가자',
-    }),
-    password: z.string().min(2, {
-        message: '비밀번호 제대로 가자',
-    }),
-    passwordCheck: z.string().min(2, {
-        message: '비밀번호 제대로 가자',
-    }),
-    phone: z.string().min(2, {
-        message: '비밀번호 제대로 가자',
-    }),
-    certificationCode: z.string().min(2, {
-        message: '비밀번호 제대로 가자',
-    }),
-});
+type Address = {
+    zonecode: string;
+    address: string;
+    addressEnglish: string;
+    addressType: 'R' | 'J';
+    userSelectedType: 'R' | 'J';
+    noSelected: 'Y' | 'N';
+    userLanguageType: 'K' | 'E';
+    roadAddress: string;
+    bname: string;
+};
 
 export default function Address() {
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            name: '',
-            email: '',
-        },
-    });
+    const open = useDaumPostcodePopup(
+        'https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js',
+    );
+    const [zipCode, setZipcode] = useState<string>('');
+    const [roadAddress, setRoadAddress] = useState<string>('');
+    const [searchAddress, setSearchAddress] = useState(false);
 
-    //& 수정 필요 (toast)
-    function onSubmit(data: z.infer<typeof FormSchema>) {
-        toast({
-            //   title: `로그인 성공!
-            // 반갑습니다 000님`,
-            description: (
-                <pre className='mt-2 w-[340px] rounded-md bg-primary p-4'>
-                    반갑다능
-                </pre>
-            ),
-        });
-    }
+    // const completeHandler = (data: any) => {
+    // 		setSearchAddress(false);
+    // 		setZipcode(data.zonecode); // 추가
+    // 		setRoadAddress(data.roadAddress); // 추가
+    // };
+
+    // const handleSearchAddress = () => {
+    // 		setSearchAddress(true);
+    // };
+
+    const handleComplete = (data: Address) => {
+        setZipcode(data.zonecode); // 추가
+        setRoadAddress(data.roadAddress); // 추가
+    };
+
+    const handleClick = () => {
+        open({ onComplete: handleComplete });
+    };
 
     return (
         <section>
@@ -71,69 +56,63 @@ export default function Address() {
                 className='mb-2 mx-auto'
             />
             <h1 className='text-center mb-[91px] font-bold'>회원가입</h1>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className='w-full'>
-                    <FormField
-                        control={form.control}
-                        name='phone'
-                        render={({ field }) => (
-                            <FormItem className='mb-6'>
-                                <FormLabel
-                                    htmlFor='address'
-                                    className='text-txt-foreground'>
-                                    주소
-                                </FormLabel>
-                                <FormControl className='flex'>
-                                    <div className='relative'>
-                                        <Input
-                                            id='address'
-                                            className='border-0 border-b-[1px] rounded-none p-[5px] text-[12px] border-txt-foreground mr-28'
-                                            type='password'
-                                            {...field}
-                                        />
-                                        <Button
-                                            type='submit'
-                                            className='font-notoSansKr absolute right-0 bottom-[0.0625rem]'
-                                            size={'sm'}
-                                            fontSize={'sm'}
-                                            fontWeight={'sm'}>
-                                            검색
-                                        </Button>
-                                    </div>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name='email'
-                        render={({ field }) => (
-                            <FormItem className='mb-8'>
-                                <FormLabel className='sr-only'>
-                                    상세 주소
-                                </FormLabel>
-                                <FormControl>
-                                    <Input
-                                        className='border-0 border-b-[1px] rounded-none p-[5px] text-[12px] border-txt-foreground'
-                                        type='email'
-                                        placeholder='상세 주소를 입력해주세요'
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage className='--destructive' />
-                            </FormItem>
-                        )}
+
+            <div>
+                <Label
+                    className='text-base text-txt-foreground'
+                    htmlFor='zipCode'>
+                    우편번호
+                </Label>
+                <div className='relative flex'>
+                    <Input
+                        id='zipCode'
+                        className='border-0 border-b-[1px] rounded-none p-[5px] border-txt-foreground self-end mr-2'
+                        type='text'
+                        placeholder='우편번호를 검색하세요'
+                        value={zipCode}
+                        onChange={e => setZipcode(e.target.value)}
                     />
                     <Button
                         type='submit'
-                        className='font-notoSansKr mt-[134px]'
-                        variant={'default'}>
-                        다음
+                        className='font-notoSansKr right-0 bottom-[0.0625rem]'
+                        size={'sm'}
+                        fontSize={'sm'}
+                        fontWeight={'sm'}
+                        onClick={handleClick}>
+                        검색
                     </Button>
-                </form>
-            </Form>
-            <Toaster />
+                </div>
+                <Label
+                    className='text-base text-txt-foreground sr-only'
+                    htmlFor='roadAddress'>
+                    도로명 주소
+                </Label>
+                <Input
+                    id='roadAddress'
+                    className='border-0 border-b-[1px] rounded-none p-[5px] text-xl border-txt-foreground mt-4'
+                    type='text'
+                    placeholder='주소를 입력하세요'
+                    value={roadAddress}
+                    onChange={e => setRoadAddress(e.target.value)}
+                />
+                <Label
+                    className='text-base text-txt-foreground sr-only'
+                    htmlFor='detailAddress'>
+                    상세 주소
+                </Label>
+                <Input
+                    id='detailAddress'
+                    className='border-0 border-b-[1px] rounded-none p-[5px] text-xl border-txt-foreground mt-4'
+                    type='text'
+                    placeholder='상세 주소를 입력하세요'
+                />
+                <Button
+                    type='submit'
+                    className='font-notoSansKr mt-[134px]'
+                    variant={'default'}>
+                    다음
+                </Button>
+            </div>
         </section>
     );
 }
