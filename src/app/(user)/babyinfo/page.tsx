3809fd832dc042edf2ddfd6199374959ac135d38
yunkooo <yunkoooooo@gmail.com>
2024-08-13 16:19:37 +0900

    ✨ [Feat] 아이 정보 DB 저장 기능 추가

--- src/app/(user)/babyinfo/page.tsx
+++ src/app/(user)/babyinfo/page.tsx
@@ -6,7 +6,6 @@ import Funnel from '@/lib/funnel/Funnel';
 import useFunnel from '@/lib/funnel/useFunnel';
 
 import { FormProvider, useForm } from 'react-hook-form';
-import { toast } from '@/components/ui/use-toast';
 import { actionBabyInfo } from '@/data/actions/babyAction';
 
 import BabyMonth from './BabyMonth';
@@ -15,43 +14,18 @@ import BabyBody from './BabyBody';
 import BabyGender from './BabyGender';
 import BabyName from './BabyName';
 import { BabyForm } from '@/types/baby';
+import { useRouter } from 'next/navigation';
 
 const steps = ['BabyName', 'BabyMonth', 'BabyGender', 'BabyBirth', 'BabyBody'];
 
-// const FormSchema = z.object({
-//     name: z
-//         .string()
-//         .min(1, 'Name is required')
-//         .max(50, 'Name must be less than 50 characters'),
-//     month: z
-//         .string()
-//         .regex(/^(0[1-9]|1[0-2])$/, 'Month must be a valid month (01-12)'),
-//     birth: z
-//         .string()
-//         .regex(
-//             /^\d{4}-\d{2}-\d{2}$/,
-//             'Birth date must be in the format YYYY-MM-DD',
-//         ),
-//     height: z
-//         .string()
-//         .min(1, 'Height is required')
-//         .regex(/^\d+$/, 'Height must be a number')
-//         .transform(val => parseInt(val, 10)),
-//     weight: z
-//         .string()
-//         .min(1, 'Weight is required')
-//         .regex(/^\d+$/, 'Weight must be a number')
-//         .transform(val => parseInt(val, 10)),
-//     gender: z.enum(['man', 'girl']),
-// });
-
 export default function Babyinfo({ params }: { params: { id: string } }) {
+    const router = useRouter();
     const { step, onNextStep } = useFunnel({ steps });
 
     const methods = useForm({
         defaultValues: {
             name: '',
-            month: '',
+            month: 0,
             birth: '',
             height: '',
             weight: '',
@@ -62,16 +36,18 @@ export default function Babyinfo({ params }: { params: { id: string } }) {
 
     //& FIXME : toast 모바일 상에서 위치 수정
     async function onSubmit(formData: BabyForm) {
-        // passwordCheck 데이터를 제외하기 위한 객체복사
-        console.log('Form Data:', formData);
-        const resData = await actionBabyInfo(formData);
+        console.log(formData);
+        if (step !== 'BabyBody') return;
+        // DB 형시에 맞추기 위해서 수정
+        const remakeFormData = {
+            extra: {
+                baby: { ...formData },
+            },
+        };
 
+        const resData = await actionBabyInfo(remakeFormData);
         if (resData.ok) {
-            toast({
-                title: `아이 정보 입력 성공!
-            		반갑습니다 ${formData.name}님`,
-                duration: 1500,
-            });
+            router.push('/login');
         } else {
             // API 서버의 에러 메시지 처리
             if ('errors' in resData) {
