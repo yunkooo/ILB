d78498d411a54cdc4aca32744486457b84e149ab
yunkooo <yunkoooooo@gmail.com>
2024-08-13 10:19:18 +0900

    ➕ [Add] Bay type 추가

---
+++ src/types/baby.ts
@@ -0,0 +1,26 @@
+export interface BabyData {}
+
+export interface BabyForm {
+    name: string;
+    month: string;
+    birth: string;
+    height: string;
+    weight: string;
+    gender: string;
+}
+
+export type Gender = 'man' | 'girl';
+
+export interface GrowType {
+    weight: number;
+    height: number;
+    date: string;
+}
+
+export interface BabyInfoData {
+    name: string;
+    month: string;
+    birth: string;
+    grow: GrowType[];
+    gender: Gender;
+}
