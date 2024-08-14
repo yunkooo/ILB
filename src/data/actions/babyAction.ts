c1168169d7b2758a70c8c82823bf1cc6f8df635b
yunkooo <yunkoooooo@gmail.com>
2024-08-14 10:54:24 +0900

    ✨ [Feat] 아이 정보 추가 기능 추가

--- src/data/actions/babyAction.ts
+++ src/data/actions/babyAction.ts
@@ -22,3 +22,67 @@ export async function actionBabyInfo(formData: RemakeBabyForm) {
     const resData = await res.json();
     return resData;
 }
+
+// 마이페이지 -> 아이 정보 수정 페이지
+export async function actionBabyBodyInfo(formData: {
+    weight: string;
+    height: string;
+}) {
+    const session = await auth();
+    const userId = session?.user.id;
+
+    const newDay = new Date();
+
+    const year = newDay.getFullYear();
+    const month = String(newDay.getMonth() + 1).padStart(2, '0');
+    const day = String(newDay.getDate()).padStart(2, '0');
+
+    const formattedDate = `${year}${month}${day}`;
+
+    // 아이 정보 가져오기
+    const resBaby = await fetch(`${SERVER}/users/${userId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'client-id': '05-ILB',
+            Authorization: `Bearer ${session?.accessToken}`,
+        },
+    });
+    const resBabyData = await resBaby.json();
+    const userExtra = resBabyData.item.extra;
+
+    const remakeFormData = {
+        extra: {
+            ...userExtra,
+            baby: {
+                ...userExtra.baby,
+                name: userExtra.baby.name,
+                month: userExtra.baby.month,
+                gender: userExtra.baby.gender,
+                birth: userExtra?.baby.birth,
+                grow: [
+                    ...userExtra.baby.grow,
+                    {
+                        weight: formData.weight,
+                        height: formData.height,
+                        date: formattedDate,
+                    },
+                ],
+            },
+        },
+    };
+
+    // 아이 정보 입력
+    const res = await fetch(`${SERVER}/users/${userId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'client-id': '05-ILB',
+            Authorization: `Bearer ${session?.accessToken}`,
+        },
+        body: JSON.stringify(remakeFormData),
+    });
+    const resData = await res.json();
+    console.log('server', resData);
+    return resData;
+}
