4aef5c216e34e584dc83e0f5b7a1711a57b372db
soheeyeo <sohee407@gmail.com>
2024-08-14 10:58:53 +0900

    ✨ [Feat] 뒤로가기 기능 구현

--- src/components/layout/BackHeader.tsx
+++ src/components/layout/BackHeader.tsx
@@ -4,10 +4,14 @@ import useScrollPosition from '@/hooks/useScroll';
 export default function BackHeader() {
     const { scrollPosition } = useScrollPosition();
 
+    const handleBack = () => {
+        window.history.back();
+    };
+
     return (
         <header
             className={`${scrollPosition ? 'bg-white' : 'bg-transparent'} fixed py-2.5 px-5 w-[375px] top-0  z-10`}>
-            <button className='w-9 h-9'>
+            <button className='w-9 h-9' onClick={handleBack}>
                 <GoArrowLeft className='mx-auto w-6 h-6 text-[#4C4646]' />
             </button>
         </header>
