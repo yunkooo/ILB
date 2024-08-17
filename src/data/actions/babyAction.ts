59ce0de4a3201fcd1e25a228f7d0c408671b6314
yunkooo <yunkoooooo@gmail.com>
2024-08-17 19:09:08 +0900

    ✨ [Feat] 아이 정보 추가 기능 수정

--- src/data/actions/babyAction.ts
+++ src/data/actions/babyAction.ts
@@ -2,10 +2,12 @@
 import { format } from 'date-fns';
 
 import { auth } from '@/auth';
-import { RemakeBabyForm } from '@/types/baby';
+import { BabyBody, RemakeBabyForm } from '@/types/baby';
+import { actionDataFetch } from './fetchAction';
 
 const SERVER = process.env.NEXT_PUBLIC_API_SERVER;
 
+// 회원가입과 아이정보 분리해서 입력할때 사용했던 action
 export async function actionBabyInfo(formData: RemakeBabyForm) {
     const session = await auth();
     const userId = session?.user.id;
@@ -26,27 +28,19 @@ export async function actionBabyInfo(formData: RemakeBabyForm) {
 }
 
 // 마이페이지 -> 아이 정보 수정 페이지
-export async function actionBabyBodyInfo(formData: {
-    weight: string;
-    height: string;
-}) {
+export async function actionBabyBodyInfo(formData: BabyBody) {
     const session = await auth();
     const userId = session?.user.id;
 
     const formattedDate = format(new Date(), 'yyyyMMdd');
 
-    // 아이 정보 가져오기
-    const resBaby = await fetch(`${SERVER}/users/${userId}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'client-id': '05-ILB',
-            Authorization: `Bearer ${session?.accessToken}`,
-        },
-    });
-    const resBabyData = await resBaby.json();
-    const userExtra = resBabyData.item.extra;
+    // 유저 정보 전체 가져오기
+    const userData = await actionDataFetch('GET', userId, session?.accessToken);
 
+    // 유저 정보에서 extra 정보만 추출
+    const userExtra = userData.item.extra;
+
+    // extra data에 새로운 아이 body data 넣어서 통째로 수정
     const remakeFormData = {
         extra: {
             ...userExtra,
@@ -69,17 +63,12 @@ export async function actionBabyBodyInfo(formData: {
     };
 
     // 아이 정보 입력
-    const res = await fetch(`${SERVER}/users/${userId}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-            'client-id': '05-ILB',
-            Authorization: `Bearer ${session?.accessToken}`,
-        },
-        body: JSON.stringify(remakeFormData),
-    });
-    const resData = await res.json();
-    console.log('server', resData);
+    const resData = await actionDataFetch(
+        'PATCH',
+        userId,
+        session?.accessToken,
+        remakeFormData,
+    );
     return resData;
 }
 
@@ -87,20 +76,7 @@ export async function getBabyData() {
     const session = await auth();
     const userId = session?.user.id;
 
-    if (session) {
-        const res = await fetch(`${SERVER}/users/${userId}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'client-id': '05-ILB',
-                Authorization: `Bearer ${session?.accessToken}`,
-            },
-        });
-        if (!res.ok) {
-            throw new Error('Failed to fetch data');
-        }
-        const resData = await res.json();
+    const resData = await actionDataFetch('GET', userId, session?.accessToken);
 
-        return resData;
-    }
+    return resData;
 }
