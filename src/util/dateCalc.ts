d4f6926247ebe8c40a389362230a328c6c08e26f
yunkooo <yunkoooooo@gmail.com>
2024-08-18 17:10:07 +0900

    ✨ [Feat] 생일로부터 날짜 계산기 기능 추가

---
+++ src/util/dateCalc.ts
@@ -0,0 +1,21 @@
+import { differenceInCalendarDays, parse } from 'date-fns';
+
+// 생일로 부터 몇일째 되는지 계산하는 함수
+export function getDayNumbers(date: string) {
+    const inputDate = parse(date, 'yyyyMMdd', new Date());
+
+    const currentDate = new Date();
+
+    const daysDifference = differenceInCalendarDays(currentDate, inputDate);
+
+    return daysDifference;
+}
+
+// 몇일째 되는 일수를 30으로 나눠서 개월수를 계산하는 함수
+export function getStepNumber(date: string) {
+    const days = getDayNumbers(date);
+
+    const step = Math.floor(days / 30);
+    console.log(step);
+    return step;
+}
