d8a4bd426177a3af16242896036ac76a6f71a154
soheeyeo <sohee407@gmail.com>
2024-08-22 02:36:58 +0900

    💄 [Styling] main 페이지 banner 수정

--- src/app/(main)/(2Banner)/ProductBannerIntro.tsx
+++ src/app/(main)/(2Banner)/ProductBannerIntro.tsx
@@ -3,13 +3,8 @@ import Image from 'next/image';
 export default function ProductBannerIntro() {
     return (
         <article className='relative'>
-            <h2 className='absolute px-5 top-[70px] text-2xl'>
-                <span className='text-3xl font-bold'>우리 아이에게만</span>
-                <br />
-                집중할 수 있도록
-            </h2>
             <Image
-                src={'/banner_2.jpg'}
+                src={'/banner_intro.webp'}
                 width={430}
                 height={753}
                 alt='intro image'
