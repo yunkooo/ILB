10c7232914b0ac9a5c9ed903e931e18990a828b2
Moong <tmdals9154@gmail.com>
2024-08-23 13:52:52 +0900

    ✨ [Feat] sidebar 애니메이션 추가

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,15 +1,23 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
+import { motion } from 'framer-motion';
+import { itemType } from './layout/SideBar';
 
-export default function LogoutButton() {
+export default function LogoutButton({
+    itemVariants,
+}: {
+    itemVariants: itemType;
+}) {
     return (
-        <button
-            className='py-[19px] px-[19px] text-left'
+        <motion.button
+            key={8}
             onClick={() => {
                 signOut({ callbackUrl: '/' });
-            }}>
+            }}
+            whileHover={{ scale: 1.1 }}
+            variants={itemVariants}>
             로그아웃
-        </button>
+        </motion.button>
     );
 }
