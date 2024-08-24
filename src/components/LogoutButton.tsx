ed910323f4f7fb8a4ad58def77bf7c5c5a303d0f
soheeyeo <sohee407@gmail.com>
2024-08-24 18:22:12 +0900

    💄 [Styling] Header, Sidebar UI 수정 및 애니메이션 추가

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -2,6 +2,8 @@
 
 import { signOut } from 'next-auth/react';
 import { motion } from 'framer-motion';
+import { TbLogout } from 'react-icons/tb';
+
 import { itemType } from './layout/SideBar';
 
 export default function LogoutButton({
@@ -14,10 +16,12 @@ export default function LogoutButton({
             key={8}
             onClick={() => {
                 localStorage.setItem('toastMessage', `로그아웃 되었습니다.`);
-                signOut({ callbackUrl: '/login' });
+                signOut({ callbackUrl: '/' });
             }}
             whileHover={{ scale: 1.1 }}
+            className='hover:bg-[#fdf9f9] flex items-center w-[90%] mx-auto rounded-lg py-3 px-5 text-left'
             variants={itemVariants}>
+            <TbLogout className='mr-2 text-[#FF7032]' />
             로그아웃
         </motion.button>
     );
