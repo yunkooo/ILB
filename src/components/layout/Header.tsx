558a69998496dd45f79baaa9c1b98c5ff5ea3743
soheeyeo <sohee407@gmail.com>
2024-08-25 01:07:23 +0900

    ✨ [Feat] Header background color 조건부 설정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,11 +9,21 @@ import SideBar from './SideBar';
 export default function Header() {
     const pathname = usePathname();
     const isMatchMain = pathname === '/';
+    const isMatchList = pathname === '/list';
     const { scrollPosition } = useScrollPosition();
+
+    //@ background color 조건부 설정
+    const backgroundClass = scrollPosition
+        ? isMatchMain
+            ? 'bg-gradient-to-t from-opacity via-gradient to-white to-90%'
+            : 'bg-white'
+        : isMatchList
+          ? 'bg-[#FFF8E1]'
+          : 'bg-transparent';
 
     return (
         <header
-            className={`${scrollPosition ? (isMatchMain ? 'bg-gradient-to-t from-opacity via-gradient to-white to-90%' : 'bg-white') : 'bg-transparent'} fixed py-2.5 px-5 max-w-screen w-full top-0  z-10`}>
+            className={`${backgroundClass} fixed py-2.5 px-5 max-w-screen w-full top-0 z-10`}>
             <nav className='flex justify-between items-center'>
                 <Link href='/'>
                     {!scrollPosition && isMatchMain ? (
