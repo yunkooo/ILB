bfee07b2e2c1ada3a09737b47168c0232b6f742a
soheeyeo <sohee407@gmail.com>
2024-08-25 14:36:19 +0900

    💄 [Styling] payment 페이지 스타일링 수정

--- src/app/(products)/order/payment/page.tsx
+++ src/app/(products)/order/payment/page.tsx
@@ -12,13 +12,13 @@ export default function PaymentInfo() {
     return (
         <section>
             <h1 className='pt-7 font-bold text-[28px]'>월간 이용권</h1>
-            <h2 className='mt-[26px] font-bold text-[20px]'>
-                월 49,000원 정기결제
+            <h2 className='mt-[26px] font-medium text-[20px]'>
+                월 <span className='font-bold'>49,000원</span> 정기결제
             </h2>
 
             <div className='mt-[54px] border px-5 py-[22px] flex flex-col gap-[22px] rounded-2xl'>
                 <div className='flex justify-between'>
-                    <p className='font-bold text-[16px] text-[#968F8F]'>
+                    <p className='font-medium text-[16px] text-txt-foreground'>
                         다음 결제 예정일
                     </p>
                     <p className='font-bold'>2024.09.23</p>
@@ -33,11 +33,11 @@ export default function PaymentInfo() {
                                 width={24}
                                 alt='toss logo'
                             />
-                            <p className='font-bold align-top text-base'>
+                            <p className='mr-1 font-bold align-top text-base'>
                                 toss
                             </p>
                         </div>
-                        <p className='font-bold text-base text-[#968F8F]'>
+                        <p className='font-medium text-base text-txt-foreground'>
                             결제 수단
                         </p>
                     </div>
