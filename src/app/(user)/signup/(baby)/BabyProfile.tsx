a937bf3b37140e3fe4260a8268c4201af0e22d1b
soheeyeo <sohee407@gmail.com>
2024-08-27 17:40:08 +0900

    💄 [Styling] BabyProfile input 스타일링 수정

--- src/app/(user)/signup/(baby)/BabyProfile.tsx
+++ src/app/(user)/signup/(baby)/BabyProfile.tsx
@@ -1,5 +1,3 @@
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { useFormContext } from 'react-hook-form';
 
 export default function BabyProfile() {
@@ -10,17 +8,21 @@ export default function BabyProfile() {
             <h1 className='text-lg text-center font-medium mb-[15vh]'>
                 아이의 사진을 넣어주세요
             </h1>
-            <Label htmlFor='attach' className='text-base text-txt-foreground'>
-                아이 프로필
-            </Label>
-            <Input
-                type='file'
-                id='attach'
-                accept='image/*'
-                placeholder='이미지를 선택하세요'
-                className='w-full px-3 py-2 border rounded-lg dark:bg-gray-700'
-                {...register('attach')}
-            />
+            <div className='flex flex-col items-center gap-4'>
+                <label
+                    htmlFor='attach'
+                    className='self-start text-txt-foreground'>
+                    아이 프로필
+                </label>
+                <input
+                    type='file'
+                    id='attach'
+                    accept='image/*'
+                    placeholder='이미지를 선택하세요'
+                    className='w-full text-lg text-txt-foreground file:w-1/3 file:mr-5 file:py-2 file:px-6 file:rounded-3xl file:border-0 file:font-medium file:bg-[#FFEBEC] file:text-txt hover:file:cursor-pointer hover:file:bg-primary-foreground'
+                    {...register('attach')}
+                />
+            </div>
         </>
     );
 }
